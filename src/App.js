bd57a4968837fde38f7c1b83d63234e42bcde4b0
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-08 17:08:52 -0700

    Captured user input in setState using onSubmit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
-import React, {useState} from "react";
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const products = [
-    {id: 198355, name: "Fancy pen", description: "Write in red or blue!", price: 1000},
-    {id: 168031, name: "Bad pen", description: "Barely works, not worth the money", price: 2000},
-    {id: 296110, name: "Fabric softener", description: "From the other leading brand ", price: 1850},  
-    {id: 183544, name: "Coffee cup", description: "Dark blue with an ergonomic handle", price: 1600},
-    {id: 174200, name: "Desk chair", description: "Your back will thank you", price: 120000},
-    {id: 203287, name: "Pillow", description: "Soft and cushiony", price: 5000},
-    {id: 107058, name: "Cactus", description: "Some greenery for your home", price: 2000},
-    {id: 226912, name: "Desk lamp", description: "Because who wants a dark office?", price: 6000}
-   ]
-   
-   const [clientInput, setClient] = useState("");
+    {
+      id: 198355,
+      name: "Fancy pen",
+      description: "Write in red or blue!",
+      price: 1000,
+    },
+    {
+      id: 168031,
+      name: "Bad pen",
+      description: "Barely works, not worth the money",
+      price: 2000,
+    },
+    {
+      id: 296110,
+      name: "Fabric softener",
+      description: "From the other leading brand ",
+      price: 1850,
+    },
+    {
+      id: 183544,
+      name: "Coffee cup",
+      description: "Dark blue with an ergonomic handle",
+      price: 1600,
+    },
+    {
+      id: 174200,
+      name: "Desk chair",
+      description: "Your back will thank you",
+      price: 120000,
+    },
+    {
+      id: 203287,
+      name: "Pillow",
+      description: "Soft and cushiony",
+      price: 5000,
+    },
+    {
+      id: 107058,
+      name: "Cactus",
+      description: "Some greenery for your home",
+      price: 2000,
+    },
+    {
+      id: 226912,
+      name: "Desk lamp",
+      description: "Because who wants a dark office?",
+      price: 6000,
+    },
+  ];
 
-   const handleUserInput = (e) => {
-     e.preventDefault();
+  const [clientInput, setClient] = useState("");
 
-     setClient(e.target.value);
-     console.log(clientInput);
-   }
+  const handleUserInput = (e) => {
+    e.preventDefault();
+    console.log("yay you made it here :)");
+    console.log(clientInput);
+  };
 
   return (
     <>
-    <form>
-      <input
-      onChange={(e) => handleUserInput(e)}
-      placeholder="Type Your Search Here" />
-      <button>Submit</button>
-      {products.map((product) => (
-      
-      `- ${product.name}`
-      )
-        )}
-        </form>
+      <form onSubmit={handleUserInput}>
+        <input onChange={(e) => setClient(e.target.value)}placeholder="Type Your Search Here" />
+        <button>Submit</button>
+        {products.map((product) => `- ${product.name}`)}
+      </form>
     </>
   );
 }
